0701d8c3076a2f3b5ca9776511891d254014c3d1
BangHann <hibatullahfarhan4@gmail.com>
2022-11-22 14:15:08 +0700

    f-paran

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginData from "../components/LoginData";
+
+const Login = () => {
+    return (
+      <div>
+        <LoginData/>
+      </div>
+    )
+  }
+  
+  export default Login
