ac8b40349ea0e86b59f0119e8ee1fed939e05d97
username_SyifaAulia0 <email_syifaauliafitriani7@gmail.com>
2022-11-25 22:52:01 +0700

    S-Syifa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,38 +8,39 @@ const Navbar = ({id}) => {
       <nav className="navbar navbar-expand-lg bg-primary">
         <div className="container-fluid">
             <a className="navbar-brand ps-5">
-                <img src={logo} alt="" width="60" height="30"/>
-                <Link to={"/News"}></Link>
+                <img src={logo} alt="" width="70" height="40"/>
+                <Link to={"/home"}></Link>
             </a>
+            
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-                      
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">              
-            <ul className="navbar-nav mx-auto">
-                <li className="nav-item">
-                    <Link to={"/trending"} style={{color: 'white', padding:"1rem"}} >Trending</Link>
-                </li>
-
-                <li className="nav-item dropdown">
-                    <div className="nav-link dropdown-toggle" href="#" role="button"  aria-expanded="false">
-                    <Link to={"/category"} style={{color: 'white', padding:"1rem"}} >Category</Link>
-                    </div>
-
+        
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mx-auto ps-5">
+                    <li className="nav-item">
+                        <Link to={"/trending"} style={{color: 'white', marginRight:"2rem"}} className="nav-link active" aria-current="page">Trending</Link>
+                    </li>
+        
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{color: 'white', marginRight:"2rem"}}>Category
+                        {/* <Link to={"/category"} style={{color: 'white'}} >Category</Link> */}
+                        </a>
+                
                     <ul className="dropdown-menu">
                         <li><Link to={"economy/:id"}><a className="dropdown-item">Economy</a></Link></li>
                         <li><Link to={`environment/${id}`}><a className="dropdown-item">Environment</a></Link></li>
                         <li><Link to={`health/${id}`}><a className="dropdown-item">Health</a></Link></li>
                         <li><Link to={`Politic/${id}`}><a className="dropdown-item">Politic</a></Link></li>
-                        <li><Link to={`eOther/${id}`}><a className="dropdown-item">Other</a></Link></li>
-                    </ul>
-                </li>
-
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <Link to={"/asean"} style={{color: 'white', padding:"1rem"}} >Asean</Link>
-                    </a>
-
+                        <li><Link to={`other/${id}`}><a className="dropdown-item">Other</a></Link></li>
+                        </ul>
+                    </li>
+                    
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{color: 'white', marginRight:"2rem"}}>Asean
+                        {/* <Link to={"/asean"} style={{color: 'white'}} >Asean</Link> */}
+                        </a>
+                    
                     <ul className="dropdown-menu">
                         <li><a className="dropdown-item" href="#">Kamboja</a></li>
                         <li><a className="dropdown-item" href="#">Laos</a></li>
@@ -52,29 +53,29 @@ const Navbar = ({id}) => {
                         <li><a className="dropdown-item" href="#">Indonesia</a></li>
                         <li><a className="dropdown-item" href="#">Brunei Darussalam</a></li>
                     </ul>
-                </li>
+                    </li>
 
-                <li className="nav-item">
-                <Link to={"/about"} style={{color: 'white', padding:"1rem"}} >About Us</Link>
-                </li>
+                    <li className="nav-item">
+                        <Link to={"/about"} style={{color: 'white'}} className="nav-link active" aria-current="page">About Us</Link>
+                    </li>
             </ul>
-            
             <form className="d-flex" role="search">
-                <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                 {/* <button className="btn btn-outline-success" type="submit">Search</button> */}
-            </form>         
-        
+            </form>
+            
+
             <ul className="nav ps-5 pe-5 justify-content-end">
                 {/* <li className="nav-item">
-                <Link to={"/formdata"} style={{color: 'white', padding:"1rem"}} >Create Data</Link>
+                    <Link to={"/formdata"} style={{color: 'white', padding:"1rem"}} className="nav-link active" >Create Data</Link>
                 </li> */}
                           
                 <li className="nav-item">
-                <Link to={"/login"} style={{color: 'white'}}>Login
-                    <MdLogin size={30} style={{color: 'white', marginRight:"1rem"}}/>
-                </Link>
-                </li>                           
-            </ul>                       
+                    <Link to={"/login"} style={{color: 'white'}} className="nav-link active">Login
+                        <MdLogin size={27} style={{color: 'white', marginRight:"1rem"}}/>
+                    </Link>
+                </li>         
+            </ul>
         </div>
         </div>           
        </nav>
