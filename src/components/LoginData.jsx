076f0ed5525ea2e8e4566a698e707af56fee9813
BangHann <hibatullahfarhan4@gmail.com>
2022-11-29 10:50:20 +0700

    f-paran

--- src/components/LoginData.jsx
+++ src/components/LoginData.jsx
@@ -1,63 +1,62 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-import { Link , useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+// import axios from 'axios';
+import { useDispatch, useSelector } from "react-redux";
+import { LoginUser, reset } from "../services/authSlice";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
     const navigate = useNavigate()
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [image,setImage] = useState('');
-    const [countryId,setCountryId] = useState('')
-    const [msg, setMsg] = useState('');
-    // const history = useHistory();
+    const dispatch = useDispatch();
+    const { user, isError, isSuccess, isLoading, message } = useSelector(
+    (state) => state.auth
+  );
 
-    const Auth = async (e) => {
-        e.preventDefault();
-        try {
-            await axios.post('https://ayfnfebe29.up.railway.app/auth/login', {
-                email: email,
-                password: password
-            });
-            navigate('/')
-        } catch (error) {
-            if (error.response) {
-                setMsg(error.response.data.msg);
-            }
-        }
+
+  useEffect(() => {
+    if (user || isSuccess) {
+      navigate("/");
     }
-    
-    return (
-        <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-            <div className="hero-body">
-                <div className="container">
-                    <div className="columns is-centered">
-                        <div className="column is-4-desktop">
-                            <form onSubmit={Auth} className="box">
-                                <p className="has-text-centered">{msg}</p>
-                                <div className="field mt-5">
-                                    <label className="label">Email</label>
-                                    <div className="controls">
-                                        <input type="text" className="input" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)} />
-                                    </div>
+    dispatch(reset());
+  }, [user, isSuccess, dispatch, navigate]);
+
+  const Auth = (e) => {
+    e.preventDefault();
+    dispatch(LoginUser({ email, password }));
+  };
+
+  return (
+    <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+        <div className="hero-body">
+            <div className="container">
+                <div className="columns is-centered">
+                    <div className="column is-4-desktop">
+                        <form onSubmit={Auth} className="box">
+                            <p className="has-text-centered">{msg}</p>
+                            <div className="field mt-5">
+                                <label className="label">Email</label>
+                                <div className="controls">
+                                    <input type="text" className="input" placeholder="Username" value={email} onChange={(e) => setEmail(e.target.value)} />
                                 </div>
-                                <div className="field mt-5">
-                                    <label className="label">Password</label>
-                                    <div className="controls">
-                                        <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
-                                    </div>
+                            </div>
+                            <div className="field mt-5">
+                                <label className="label">Password</label>
+                                <div className="controls">
+                                    <input type="password" className="input" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
                                 </div>
-                                <div className="field mt-5">
-                                    <button className="button is-success is-fullwidth">Login</button>
-                                </div>
-                            </form>
-                            <p>Don't have an account? <Link to={"/register"}>Register here</Link></p> 
-                        </div>
+                            </div>
+                            <div className="field mt-5">
+                                <button className="button is-success is-fullwidth">Login</button>
+                            </div>
+                        </form>
+                        <p>Don't have an account? <Link to={"/register"}>Register here</Link></p> 
                     </div>
                 </div>
             </div>
-        </section>
-    )
+        </div>
+    </section>
+)
 }
 
- 
-export default Login ;
+export default Login;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,9 @@ const Navbar = ({id}) => {
       <nav className="navbar navbar-expand-lg bg-primary">
         <div className="container-fluid">
             <a className="navbar-brand ps-5">
-                <img src={logo} alt="" width="70" height="40"/>
-                <Link to={"/home"}></Link>
+                <Link to={"/"}>
+                    <img src={logo} alt="" width="70" height="40"/>
+                </Link>
             </a>
             
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
