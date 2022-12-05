7dc8b1aec2f702689d09c65edbbcf4dc44e24e91
BangHann <hibatullahfarhan4@gmail.com>
2022-12-05 17:36:50 +0700

    f-paran

--- src/components/NavbarAdmin.jsx
+++ src/components/NavbarAdmin.jsx
@@ -6,108 +6,104 @@ import axios from "axios";
 
 const Logout = () => {
     localStorage.clear();
-    location.href='/';
+    location.href = '/';
 }
 
-const NavbarAdmin = ({}) => {
-    const [news, setNews] = useState([]);
+const NavbarAdmin = ({setNews, ...rest}) => {
     const [keyword, setKeyword] = useState("");
     const [title, setTitle] = useState("");
 
     useEffect(() => {
-        axios.get(`https://ayfnfebe29.up.railway.app/news/search?title=${keyword}`).then((res)=>{
+        axios.get(`https://ayfnfebe29.up.railway.app/news/search?title=${keyword}`).then((res) => {
             setNews(res.data)
         })
-    }, []);
-    // console.log({news, keyword})
+    }, [title, keyword]);
 
     const searchByTitle = (e) => {
         e.preventDefault();
         setKeyword(title);
-        // setNews(res.data)
+        axios.get(`https://ayfnfebe29.up.railway.app/news/search?title=${keyword}`).then((res) => {
+            setNews(res.data)
+        })
     }
 
     return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-primary">
-        <div className="container-fluid">
-            <span className="navbar-brand ps-5">
-                <Link to={"/homeadmin"}>
-                    <img src={logo} alt="" width="70" height="40"/>
-                </Link>
-            </span>
-            
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-        
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        
-                    <li className="nav-item">
-                            <Link to={"/category"} style={{color: 'white', marginRight:"2rem"}} className="nav-link active">Category</Link>
-                    </li>
+        <>
+            <nav className="navbar navbar-expand-lg bg-primary">
+                <div className="container-fluid">
+                    <span className="navbar-brand ps-5">
+                        <Link to={"/homeadmin"}>
+                            <img src={logo} alt="" width="70" height="40" />
+                        </Link>
+                    </span>
 
-                    <li className="nav-item">
-                            <Link to={"/country"} style={{color: 'white', marginRight:"2rem"}} className="nav-link active">Asean</Link>
-                    </li>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                    <li className="nav-item">
-                            <Link to={"/formadmin"} style={{color: 'white', marginRight:"2rem"}} className="nav-link active">Dashboard</Link>
-                    </li>
-            </ul>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-            <ul className="navbar-nav pe-5 justify-content-end">
-            <form className="d-flex" role="search" onSubmit={searchByTitle}>
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-primary" type="submit" onClick={searchByTitle}  style={{color: 'white'}}>Search</button>
-                </form>
-                <li className="nav-item">
-                    {
-                    localStorage.getItem('token') 
-                    ?
-                        <>
-                        {/* <button className="btn nav-link text-white" > */}
-                            {/* <Link to={"/data"} style={{color: 'white'}} className="nav-link active" >Create Data</Link> */}
-                        {/* </button> */}
+                            <li className="nav-item">
+                                <Link to={"/category"} style={{ color: 'white', marginRight: "2rem" }} className="nav-link active">Category</Link>
+                            </li>
 
-                        <button className="btn nav-link text-white" data-bs-toggle="modal" data-bs-target="#logout">
-                            Logout <MdLogout size={27} style={{color: 'white'}}/>
-                        </button>
-                        
-                        <div className="modal fade" id="logout" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="logoutLabel" aria-hidden="true">
-                            <div className="modal-dialog">
-                                <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title" id="logoutLabel">Confirm Logout</h5>
-                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div className="modal-body">
-                                    Are you sure you want to exit?
-                                </div>
-                                <div className="modal-footer">
-                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelled</button>
-                                    <button type="button" className="btn btn-primary" onClick={Logout}>Confirmation</button>
-                                </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        </> 
-                    :
-                        <>
-                            <Link to={"/login"} style={{color: 'white'}} className="nav-link active">Login
-                                <MdLogin size={27} style={{color: 'white', marginRight:"1rem"}}/>
-                            </Link>
-                        </>
-                    }
-                </li>         
-            </ul>
-        </div>
-        </div>    
-       </nav>
-</>
+                            <li className="nav-item">
+                                <Link to={"/country"} style={{ color: 'white', marginRight: "2rem" }} className="nav-link active">Asean</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link to={"/formadmin"} style={{ color: 'white', marginRight: "2rem" }} className="nav-link active">Dashboard</Link>
+                            </li>
+                        </ul>
+
+                        <ul className="navbar-nav pe-5 justify-content-end">
+                            <form className="d-flex" role="search" onSubmit={searchByTitle}>
+                                <input className="form-control me-2" onChange={(e) => setTitle(e.target.value)} type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-secondary bg-gradient" type="submit" style={{ color: 'white' }}>Search</button>
+                            </form>
+                            <li className="nav-item">
+                                {
+                                    localStorage.getItem('token')
+                                        ?
+                                        <>
+                                            <button className="btn nav-link text-white" data-bs-toggle="modal" data-bs-target="#logout">
+                                                Logout <MdLogout size={27} style={{ color: 'white' }} />
+                                            </button>
+
+                                            <div className="modal fade" id="logout" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="logoutLabel" aria-hidden="true">
+                                                <div className="modal-dialog">
+                                                    <div className="modal-content">
+                                                        <div className="modal-header">
+                                                            <h5 className="modal-title" id="logoutLabel">Confirm Logout</h5>
+                                                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div className="modal-body">
+                                                            Are you sure you want to exit?
+                                                        </div>
+                                                        <div className="modal-footer">
+                                                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelled</button>
+                                                            <button type="button" className="btn btn-primary" onClick={Logout}>Confirmation</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </>
+                                        :
+                                        <>
+                                            <Link to={"/login"} style={{ color: 'white' }} className="nav-link active">Login
+                                                <MdLogin size={27} style={{ color: 'white', marginRight: "1rem" }} />
+                                            </Link>
+                                        </>
+                                }
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
-  }
-  
-  export default NavbarAdmin
+}
+
+export default NavbarAdmin
