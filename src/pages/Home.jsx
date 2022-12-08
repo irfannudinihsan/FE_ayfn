4ce2c18d5f78db9f6716519177f120cccbc7dc19
username_SyifaAulia0 <email_syifaauliafitriani7@gmail.com>
2022-12-08 15:39:51 +0700

    S-Syifa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import News from './News'
 import orang from "../assets/orang.png"
+import peopleTalking from "../assets/people-talking.png"
 import { useState } from 'react'
 
 const Home = () => {
@@ -13,9 +14,21 @@ const Home = () => {
     return (
       <div>
           <Navbar setNews={setNews}/>
-            <div className='container-fluid d-flex justify-content-center'>
-              <img src={orang}  className=""/>
+          <div className="container">
+            <div className="row">
+              <div className="col-4">
+                <h1 className='fw-bold text-start' size={80} style={{color: "blue", marginTop:"8rem", size:"90"}}>ASEAN YOUTH FORUM NEWS</h1>
+                <h3 className='fw-bold text-xl-start'>Valid News for youth in ASEAN</h3>
+              </div>
+              <div className="col-8 text-end">
+                <img src={peopleTalking} width="570"/>
+              </div>
             </div>
+          </div>
+
+            {/* <div className='container-fluid d-flex justify-content-center'>
+              <img src={orang}  className=""/>
+            </div> */}
           
           <News news={news}/>
           <Footer/>
--- src/pages/HomeAdmin.jsx
+++ src/pages/HomeAdmin.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Footer from '../components/Footer'
 import NavbarAdmin from '../components/NavbarAdmin'
 import News from './News'
-
+import peopleTalking from '../assets/people-talking.png'
 import NewsAdmin from './NewsAdmin'
 
 
@@ -17,6 +17,18 @@ const HomeAdmin = (props) => {
       <div>
         {console.log(news)}
           <NavbarAdmin  setNews={setNews}/>
+
+          <div className="container">
+            <div className="row">
+              <div className="col-4">
+                <h1 className='fw-bold text-start' size={80} style={{color: "blue", marginTop:"8rem", size:"90"}}>ASEAN YOUTH FORUM NEWS</h1>
+                <h3 className='fw-bold text-xl-start'>Valid News for youth in ASEAN</h3>
+              </div>
+              <div className="col-8 text-end">
+                <img src={peopleTalking} width="570"/>
+              </div>
+            </div>
+          </div>
           <NewsAdmin news={news}/>
 
           <Footer/>
